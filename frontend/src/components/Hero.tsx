4db8f85d4d77efb9743db405a0ef47cf709f6172
low-perry <dariohaxhiraj@hotmail.com>
2024-05-28 14:18:09 +0200

    create dthe basic layout for the app, fixed the tailwind.config file. Implemented, Header and footer.

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+    return (
+        <div className="bg-blue-800 pb-16">
+            <div className="container mx-auto flex flex-col gap-2">
+                <h1 className="text-4xl text-white font-bold tracking-tight text-center">
+                    Welcome to Book.com
+                </h1>
+                <p className="text 2-xl text-white text-center">
+                    The best place to explore...
+                </p>
+            </div>
+        </div>
+    );
+};
+export default Hero;
