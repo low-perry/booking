4f73061879321f4390d0971ad61590db68af29c8
low-perry <dariohaxhiraj@hotmail.com>
2024-05-29 13:08:00 +0200

    Created the user model and schema, implemented registration and login on the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth"; // Import the authRoutes module
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -9,11 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-app.get("/api/test", async (req : Request, res: Response) => {
-  res.json({message: "Hello World!"});
-});
-
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.listen(7000, () => {
     console.log("Server is running on port 7000");
 });
